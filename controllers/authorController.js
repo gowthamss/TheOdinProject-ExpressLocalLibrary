68f45adef18134ca3a5b7495d638931266da3ea5
bssgowtham <55312214+bssgowtham@users.noreply.github.com>
2021-12-05 23:59:49 +0530

    created author_list, book_list, bookinstance_list, genre_detail and genre_list controller methods and views

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,15 @@
 const Author = require('../models/author');
 
 // Display list of authors
-exports.author_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author List');
+exports.author_list = function(req, res, next) {
+    Author.find()
+        .sort({ 'family_name': 1 })
+        .exec(function(err, list_authors) {
+            if (err) { return next(err); }
+
+            // Successful, so render
+            res.render('author_list', { title: 'Author List', author_list: list_authors });
+        })
 }
 
 // Display detail page for a specific author
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -14,6 +15,12 @@ BookInstanceSchema.virtual('url')
     .get(function() {
         return `/catalog/bookinstance/${this._id}`;
     });
+
+// Virtual for formatted due date
+BookInstanceSchema.virtual('due_back_formatted')
+    .get(function() {
+        return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+    })
 
 // Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
